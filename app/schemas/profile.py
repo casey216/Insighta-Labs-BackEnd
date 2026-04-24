cecdc7a49de97846a9dce1b771875192525667d9
Kenechi Nzewi <caseynzewi@gmail.com>
2026-04-24 10:56:51 +0100

    feat(profile): add ProfileQueryBuilder

--- app/schemas/profile.py
+++ app/schemas/profile.py
@@ -14,6 +14,15 @@ class AgeGroup(StrEnum):
     ADULT = "adult"
     SENIOR = "senior"
 
+class SortEnum(StrEnum):
+    AGE = "age"
+    CREATED_AT = "created_at"
+    GENDER_PROBABILITY = "gender_probability"
+
+class OrderByEnum(StrEnum):
+    ASCENDING = "asc"
+    DESCENDING = "desc"
+
 class ProfileCreate(BaseModel):
     name: str
     gender: Gender
@@ -33,3 +42,25 @@ class ProfileUpdate(BaseModel):
     country_id: str | None = None
     country_name: str | None = None
     country_probability: float | None = None
+
+class FilterParams(BaseModel):
+    gender: list[Gender] | None = None
+    age_group: list[AgeGroup] | None = None
+    country_id: str | None = None
+    min_age: int | None = None
+    max_age: int | None = None
+    min_gender_probability: float | None = None
+    min_country_probability: float | None = None
+
+class SortParams(BaseModel):
+    sort_by: SortEnum | None = None
+    order_by: OrderByEnum = OrderByEnum.ASCENDING
+
+class PaginationParams:
+    def __init__(self, page: int = 1, limit: int = 100):
+        self.page = max(page, 1)
+        self.limit = min(limit, 100)
+
+    @property
+    def offset(self):
+        return (self.page - 1) * self.limit
--- app/services/profile_service.py
+++ app/services/profile_service.py
@@ -1,10 +1,11 @@
 from uuid import UUID
 
-from sqlalchemy.orm import Session
+from sqlalchemy import asc, desc
+from sqlalchemy.orm import Session, Query
 from sqlalchemy.exc import IntegrityError
 
 from app.models.profile import Profile
-from app.schemas.profile import ProfileCreate, ProfileUpdate
+from app.schemas.profile import ProfileCreate, ProfileUpdate, Gender, AgeGroup, SortParams
 
 
 class ProfileService:
@@ -64,3 +65,55 @@ class ProfileService:
         db.commit()
 
         return True
+    
+
+class ProfileQueryBuilder:
+    """Query buider for Profile service"""
+    def __init__(self, query: Query):
+        self.query = query
+        self.sort_column = None
+
+    def filter_gender(self, genders: list[Gender]):
+        if genders:
+            self.query = self.query.filter(Profile.gender.in_(genders))
+        return self
+
+    def filter_age(self, min_age: int, max_age: int):
+        if min_age:
+            self.query = self.query.filter(Profile.age >= min_age)
+        if max_age:
+            self.query = self.query.filter(Profile.age <= max_age)
+        return self
+    
+    def filter_age_group(self, age_group: AgeGroup):
+        if age_group:
+            self.query = self.query.filter(Profile.age_group == age_group.lower())
+
+    def filter_country_id(self, country_id: str):
+        if country_id:
+            self.query = self.query.filter(Profile.country_id == country_id.upper())
+
+    def filter_country_probability(self, min_country_p: float, max_country_p: float):
+        if min_country_p:
+            self.query = self.query.filter(Profile.country_probability == min_country_p)
+        if max_country_p:
+            self.query = self.query.filter(Profile.country_probability == max_country_p)
+
+    def sort_by(self, sort_params: SortParams):
+        if sort_params.sort_by:
+            value = sort_params.sort_by.value
+            if value == "age":
+                self.sort_column = Profile.age
+            if value == "created_at":
+                self.sort_column = Profile.created_at
+            if value == "gender_probability":
+                self.sort_column = Profile.gender_probability
+
+        if self.sort_column:
+            if sort_params.order_by == "asc":
+                self.query = self.query.order_by(asc(self.sort_column))
+            else:
+                self.query = self.query.order_by(desc(self.sort_column))
+
+    def build(self):
+        return self.query
