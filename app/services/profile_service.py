01f66aed8168b7f0cc5bdcbf7d014c4a87a4b1f6
Kenechi Nzewi <caseynzewi@gmail.com>
2026-04-24 12:28:10 +0100

    feat(services): add filtering, sortinng and pagination to profile service

--- app/services/profile_service.py
+++ app/services/profile_service.py
@@ -1,3 +1,4 @@
+from typing import Any
 from uuid import UUID
 
 from sqlalchemy import asc, desc
@@ -5,7 +6,11 @@ from sqlalchemy.orm import Session, Query
 from sqlalchemy.exc import IntegrityError
 
 from app.models.profile import Profile
-from app.schemas.profile import ProfileCreate, ProfileUpdate, Gender, AgeGroup, SortParams
+from app.schemas.profile import (
+    ProfileCreate, ProfileUpdate, 
+    Gender, AgeGroup, SortParams,
+    FilterParams, PaginationParams
+)
 
 
 class ProfileService:
@@ -33,8 +38,31 @@ class ProfileService:
         return db.query(Profile).filter(Profile.name == name).first()
     
     @staticmethod
-    def get_all_profiles(db: Session) -> list[Profile]:
-        return db.query(Profile).all()
+    def get_all_profiles(
+        filter_params: FilterParams,
+        sort_params: SortParams,
+        p: PaginationParams,
+        db: Session
+        ) -> dict[str, Any]:
+        q = db.query(Profile)
+        q = ProfileQueryBuilder(q)
+        q = q.filter_age(filter_params.min_age, filter_params.max_age)
+        q = q.filter_age_group(filter_params.age_group)
+        q = q.filter_country_id(filter_params.country_id)
+        q = q.filter_country_probability(filter_params.min_country_probability)
+        q = q.filter_gender(filter_params.gender)
+        q = q.filter_gender_probability(filter_params.min_gender_probability)
+        q = q.sort_by(sort_params)
+        q = q.build()
+
+        total = q.count()
+        profiles = q.offset(p.offset).limit(p.limit).all()
+
+        return {
+            "total": total,
+            "data": profiles
+        }
+        
     
     @staticmethod
     def update_profile(db: Session, profile_id: UUID, profile: ProfileUpdate) -> Profile | None:
@@ -69,38 +97,42 @@ class ProfileService:
 
 class ProfileQueryBuilder:
     """Query buider for Profile service"""
-    def __init__(self, query: Query):
+    def __init__(self, query: Query[Profile]):
         self.query = query
         self.sort_column = None
 
-    def filter_gender(self, genders: list[Gender]):
+    def filter_gender(self, genders: list[Gender] | None):
         if genders:
             self.query = self.query.filter(Profile.gender.in_(genders))
         return self
 
-    def filter_age(self, min_age: int, max_age: int):
+    def filter_age(self, min_age: int | None, max_age: int | None):
         if min_age:
             self.query = self.query.filter(Profile.age >= min_age)
         if max_age:
             self.query = self.query.filter(Profile.age <= max_age)
         return self
     
-    def filter_age_group(self, age_group: AgeGroup):
-        if age_group:
-            self.query = self.query.filter(Profile.age_group == age_group.lower())
+    def filter_age_group(self, age_groups: list[AgeGroup] | None):
+        if age_groups:
+            self.query = self.query.filter(Profile.age_group.in_(age_groups))
+        return self
 
-    def filter_country_id(self, country_id: str):
+    def filter_country_id(self, country_id: str | None):
         if country_id:
             self.query = self.query.filter(Profile.country_id == country_id.upper())
+        return self
 
-    def filter_country_probability(self, min_country_p: float):
+    def filter_country_probability(self, min_country_p: float | None):
         if min_country_p:
             self.query = self.query.filter(Profile.country_probability == min_country_p)
-        
-    def filter_gender_probability(self, min_gender_p: float):
+        return self
+    
+    def filter_gender_probability(self, min_gender_p: float | None):
         if min_gender_p:
             self.query = self.query.filter(Profile.country_probability == min_gender_p)
-
+        return self
+    
     def sort_by(self, sort_params: SortParams):
         if sort_params.sort_by:
             value = sort_params.sort_by.value
@@ -116,6 +148,7 @@ class ProfileQueryBuilder:
                 self.query = self.query.order_by(asc(self.sort_column))
             else:
                 self.query = self.query.order_by(desc(self.sort_column))
+        return self
 
     def build(self):
         return self.query
